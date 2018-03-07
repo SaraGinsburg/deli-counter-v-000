05b7fed36a929581d96fb6786f4172e40af75ac3
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 18:54:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
-require "pry"
+require "pryde"
 def line(deli)
   if deli.size == 0
-    binding.pry
     puts "The line is currently empty."
   else
     display = []
     deli.each_with_index do |person, idx|
       display << " #{idx + 1}. #{person}"
     end
+    binding.pry
     result = "The line is currently:" + display.join('')
   end
   result
