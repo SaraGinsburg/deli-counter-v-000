31a9ffa4250cce0526dd30b075de536d63fdb23e
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 18:11:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+deli = []
+
+def line(deli)
+  if deli.length = 0
+    puts "The line is currently empty."
+  else
+    display_line(deli)
+  end
+end
+
+def display_line(deli)
+  display = []
+  deli.each_with_index do |person, idx|
+    display << " #{idx + 1}. #{person}"
+  end
+  "The line is currently: " + display.join(',')
+end
