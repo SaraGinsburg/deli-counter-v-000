10cf485e2ebc79e948892dc19be541008be349a7
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 18:48:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,11 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else
-    display_line(deli)
+    display = []
+    deli.each_with_index do |person, idx|
+      display << " #{idx + 1}. #{person}"
+    end
+    "The line is currently:" + display.join('')
   end
 end
 
-def display_line(deli)
-  display = []
-  deli.each_with_index do |person, idx|
-    display << " #{idx + 1}. #{person}"
-  end
-  "The line is currently:" + display.join('')
-end
